480bdbe61fca08a807365cb2bd4706a75c57c4a3
Sh3Ns <50599143+Sh3Ns@users.noreply.github.com>
2025-10-15 15:58:39 -0300

    Update creaturescripts_ferumbras_mortal_shell_death.lua

--- data-global/scripts/quests/ferumbras_ascension/creaturescripts_ferumbras_mortal_shell_death.lua
+++ data-global/scripts/quests/ferumbras_ascension/creaturescripts_ferumbras_mortal_shell_death.lua
@@ -2,40 +2,17 @@ local ferumbrasMortalShell = CreatureEvent("FerumbrasMortalShell")
 
 local config = AscendingFerumbrasConfig
 
-_G.ferumbrasMortalShellSpawning = _G.ferumbrasMortalShellSpawning or false
-
 function ferumbrasMortalShell.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
 	if creature:getName():lower() ~= "destabilized ferumbras" then
 		return true
 	end
 
-	if _G.ferumbrasMortalShellSpawning then
-		return true
-	end
-
-	-- Verificar se o boss já existe na área
-	local spectators = Game.getSpectators(config.bossPos, false, false, 15, 15, 15, 15)
-	for _, spec in pairs(spectators) do
-		if spec:isMonster() and spec:getName():lower() == "ferumbras mortal shell" then
-			return true -- Boss já existe, não spawnar outro
-		end
-	end
-
-	_G.ferumbrasMortalShellSpawning = true
-
 	local monster = Game.createMonster("Ferumbras Mortal Shell", config.bossPos, true, true)
 	if not monster then
-		_G.ferumbrasMortalShellSpawning = false
 		return true
 	end
-
 	monster:say("AAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHH!", TALKTYPE_MONSTER_SAY)
 	lasthitkiller:say("FINALY YOU FORCED FERUMBRAS BACK INTO A MORTAL FORM - HE IS NOT AMUSED!", TALKTYPE_MONSTER_SAY, nil, nil, config.bossPos)
-
-	addEvent(function()
-		_G.ferumbrasMortalShellSpawning = false
-	end, 2000)
-
 	return true
 end
 
